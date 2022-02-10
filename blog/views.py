8959abdff1e8a0ba14d471a52a93678d7218b392
MichuSz <szmania_michal@yahoo.com>
2022-02-10 14:49:36 +0100

    Add comment,tags

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from SuperBlog.secret import mail
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
@@ -11,7 +11,8 @@ class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = 'blog/post/list.' \
+                    'html'
 
 
 # def post_list(request):
@@ -30,7 +31,17 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':                                #komentarz zostal opublikowany
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)       #utworzenie obieku Comment
+            new_comment.post = post                             #przypisanie komentarza do biezacego posta
+            new_comment.save()                                  #zapisanie komentarza w bazie danych
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments':comments,'comment_form':comment_form})
 
 
 def post_share(request, post_id):
